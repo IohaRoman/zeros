419521db051ecef0082d79abfdfb10717a5d1d2f
IohaRoman <ioxa147@gmail.com>
2018-03-02 17:38:14 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+    var countZeros = 0;
+
+        for (var i = 5; i <= number; i = i + 5) {
+            var countIteration = i;
+
+            while  (countIteration % 5 === 0) {
+                countIteration = countIteration / 5;
+                countZeros++;
+            }
+        }
+
+        return countZeros;
+};
